2d69137417b3080c1b5dc149794c032ea1a625ba
Naledi Madlopha <naledi.madlopha@gmail.com>
2018-09-11 20:38:00 +0200

    Implement code to submit a typed message to the UI

--- app/src/main/java/com/naledimadlopha/gdgbot/app/view/MainActivity.kt
+++ app/src/main/java/com/naledimadlopha/gdgbot/app/view/MainActivity.kt
@@ -2,19 +2,36 @@ package com.naledimadlopha.gdgbot.app.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.view.View
 import com.naledimadlopha.gdgbot.app.R
+import com.naledimadlopha.gdgbot.app.model.BaseMessage
+import com.naledimadlopha.gdgbot.app.view.MessageListAdapter.Companion.SELF
 import kotlinx.android.synthetic.main.content_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var adapter: MessageListAdapter = MessageListAdapter(ArrayList())
+    private val adapter: MessageListAdapter = MessageListAdapter(ArrayList())
+    private lateinit var simpleDateFormat: SimpleDateFormat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        simpleDateFormat = SimpleDateFormat(getString(R.string.message_time_format), Locale.getDefault())
 
         messageListRecyclerView.adapter = adapter
     }
+
+    fun sendButtonClicked(view: View) {
+        val message = messageEditorEditText.text.toString()
+
+        if (!TextUtils.isEmpty(message)) {
+            adapter.addMessage(BaseMessage(message, SELF, simpleDateFormat.format(Date())))
+            messageEditorEditText.setText("")
+        }
+    }
+
 
 }
