0b64378db5950579b90916596a45326a4be2b4aa
Naledi Madlopha <naledi.madlopha@gmail.com>
2018-09-11 14:34:28 +0200

    Implement an App Bar Layout
    * Implement an app bar layout with a GDG logo icon and Title
    * Move the main activity to a view package which will package all UI
    related code

--- app/src/main/java/com/naledimadlopha/gdgbot/app/view/MainActivity.kt
+++ app/src/main/java/com/naledimadlopha/gdgbot/app/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.naledimadlopha.gdgbot.app
+package com.naledimadlopha.gdgbot.app.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.naledimadlopha.gdgbot.app.R
 
 class MainActivity : AppCompatActivity() {
 
